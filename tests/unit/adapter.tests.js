f92fd180ca5df1acbcf170d68d5bb8aecdec960f
DullReferenceException <jpage@godaddy.com>
2014-06-15 15:07:08 -0700

    Added ping and close functionality

--- tests/unit/adapter.tests.js
+++ tests/unit/adapter.tests.js
@@ -4,6 +4,7 @@ var sinon = require('sinon');
 var chai = require('chai');
 var expect = chai.expect;
 var proxyquire = require('proxyquire');
+var async = require('async');
 
 chai.use(require('sinon-chai'));
 
@@ -14,7 +15,7 @@ describe('The CassandraAdapter', function() {
   var mockCassandra, mockCassandraClient;
 
   beforeEach(function() {
-    mockCassandraClient = {};
+    mockCassandraClient = { shutdown: sinon.stub().yields(null) };
     mockCassandra = { Client: sinon.stub().returns(mockCassandraClient) };
 
     CassandraAdapter = proxyquire('../../lib/adapter', {
@@ -24,10 +25,10 @@ describe('The CassandraAdapter', function() {
 
   describe('constructor', function() {
     it('requires either a host or hosts option', function() {
-      expect(function() { var adapter = new CassandraAdapter({}); }).to.throw(Error);
+      expect(function() { new CassandraAdapter({}); }).to.throw(Error);
     });
     it('requires a non-empty path', function() {
-      expect(function() { var adapter = new CassandraAdapter({}); }).to.throw(Error);
+      expect(function() { new CassandraAdapter({}); }).to.throw(Error);
     });
   });
 
@@ -52,4 +53,40 @@ describe('The CassandraAdapter', function() {
       });
     });
   });
+
+  describe('ping method', function() {
+    it('always succeeds', function(done) {
+      var adapter = new CassandraAdapter({ path: "/keyspace", hosts: "host1:2304,host2,host3" });
+      adapter.ping(function(err) {
+        expect(err).to.be.null;
+        done();
+      });
+    });
+  });
+
+  describe('close method', function() {
+
+    var adapter;
+
+    beforeEach(function() {
+      adapter = new CassandraAdapter({ path: "/keyspace", hosts: "host1:2304,host2,host3" });
+    });
+
+    it('does nothing if a connection has not been opened', function(done) {
+      adapter.close(function(err) {
+        expect(err).to.be.null;
+        done();
+      });
+    });
+
+    it('calls shutdown for the underlying client if connected', function(done) {
+      async.series([
+        adapter.connect.bind(adapter),
+        adapter.close.bind(adapter)
+      ], function() {
+        expect(mockCassandraClient.shutdown).to.have.been.called;
+        done();
+      });
+    });
+  })
 });
